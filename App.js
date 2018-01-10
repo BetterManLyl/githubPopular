6c4ccaf405034837139a3bbe69e52f77df2f7a1d
lyl953147712 <953147712@qq.com>
2018-01-10 17:04:09 +0800

    图片上传

--- App.js
+++ App.js
@@ -33,6 +33,7 @@ export default class App extends Component<{}> {
 
     render() {
         return (
+
             <View style={styles.container}>
                <Image source={require('./images/ic_trending.png')}/>
 
