e3691bdf6c318891b2a0cb81f80a2cd4023e4731
lyl953147712 <953147712@qq.com>
2018-01-10 17:03:31 +0800

    图片上传

--- App.js
+++ App.js
@@ -4,54 +4,95 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Image
+
 } from 'react-native';
+import TabNavigator from 'react-native-tab-navigator'
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
+    ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
+    android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
 export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+    constructor() {
+        super()
+        this.state = {
+            selectedTab: 'home'
+
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+               <Image source={require('./images/ic_trending.png')}/>
+
+                {/*<TabNavigator>*/}
+                    {/*<TabNavigator.Item*/}
+                        {/*selected={this.state.selectedTab === 'home'}*/}
+                        {/*title='home'*/}
+                        {/*renderIcon={() => <Image source={require('./images/ic_polular.png')}/>}*/}
+                    {/**/}
+                        {/*renderSelectedIcon={() => <Image source={require('./images/ic_polular.png')}/>}*/}
+                        {/*badgeText='1'*/}
+                        {/*onPress={() => this.setState({*/}
+                            {/*selectedTab: 'home'*/}
+                        {/*})}*/}
+                    {/*>*/}
+                        {/*<View style={styles.pag1}></View>*/}
+                    {/*</TabNavigator.Item>*/}
+
+                    {/*<TabNavigator.Item*/}
+                        {/*selected={this.state.selectedTab === 'profile'}*/}
+                        {/*title='profile'*/}
+                        {/*renderIcon={() => <Image source={require('./images/ic_trending.png.png')}/>}*/}
+                        {/*renderSelectedIcon={() => <Image source={require('./images/ic_trending.png')}/>}*/}
+                        {/*badgeText='1'*/}
+                        {/*onPress={() => this.setState({*/}
+                            {/*selectedTab: 'profile'*/}
+                        {/*})}*/}
+                    {/*>*/}
+                        {/*<View style={styles.pag2}></View>*/}
+                    {/*</TabNavigator.Item>*/}
+                {/*</TabNavigator>*/}
+
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    pag1: {
+        flex: 1,
+        backgroundColor: 'red'
+
+    },
+    pag2: {
+        flex: 1,
+        backgroundColor: 'yellow'
+
+    }
 });
